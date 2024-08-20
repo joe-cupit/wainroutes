93a0507841864a1bc93ec09abc4fed78c4180f1a
Joe <jwcupit@gmail.com>
2024-08-20 22:34:29 +0100

    Add walk start locations to map and toggling between marker types

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,21 +2,53 @@ import "../styles/home.css";
 
 import { Link } from "react-router-dom";
 import { LakeMap } from "./map";
+import { useState } from "react";
 
 
 export function HomePage() {
+
+  const [showWainwrights, setShowWainwrights] = useState(false)
+  const [showWalkroutes, setShowWalkroutes] = useState(true)
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    for (let button of document.getElementsByClassName("active")) {
+      button.classList.remove("active");
+    }
+    e.target.classList.add("active");
+
+    switch (e.target.innerText) {
+      case "walks":
+        setShowWainwrights(false);
+        setShowWalkroutes(true);
+        break;
+      case "mountains":
+        setShowWainwrights(true);
+        setShowWalkroutes(false);
+        break;
+      default:
+        setShowWainwrights(false);
+        setShowWalkroutes(false);
+    }
+  }
+
   return (
   <main className="home-page">
     <header>
       <h1>Wainwright Walks</h1>
       <div className="home-page--links">
-        <Link to="/walks">walks</Link>
+        {/* <Link to="/walks">walks</Link>
         <Link to="/mountains">mountains</Link>
         <Link to="/weather">weather</Link>
-        <Link to="/more">more</Link>
+        <Link to="/more">more</Link> */}
+        <button className={showWalkroutes && "active"} onClick={handleClick}>walks</button>
+        <button className={showWainwrights && "active"} onClick={handleClick}>mountains</button>
+        <button onClick={handleClick}>weather</button>
+        <button onClick={handleClick}>more</button>
       </div>
     </header>
-    <LakeMap />
+    <LakeMap showWainwrights={showWainwrights} showWalkroutes={showWalkroutes}/>
   </main>
   )
 }
--- src/pages/map.jsx
+++ src/pages/map.jsx
@@ -1,12 +1,17 @@
-import { useEffect, useState } from "react";
 import "../styles/map.css";
 
-import { Map, Marker } from "pigeon-maps";
+import { useEffect, useState } from "react";
+import { Map, Marker, ZoomControl } from "pigeon-maps";
+import { maptiler } from 'pigeon-maps/providers';
+
+// import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 
 
-export function LakeMap() {
+export function LakeMap({ showWainwrights=false, showWalkroutes=false }) {
 
-  const [center, setCenter] = useState([54.49, -3.05]);
+  const [center, setCenter] = useState([54.55, -3.09]);
+  const [zoom, setZoom] = useState(10.65)
+  // const maptilerProvider = maptiler(process.env.REACT_APP_MAP_API_KEY, "topo-v2")
 
   const [wainwrights, setWainwrights] = useState();
   useEffect(() => {
@@ -14,17 +19,37 @@ export function LakeMap() {
       .then(response => response.text())
       .then(responseText => {
         setWainwrights(JSON.parse(responseText));
-        console.log(JSON.parse(responseText));
       });
   }, []);
 
-  return (
+  const [walkRoutes, setWalkRoutes] = useState();
+  useEffect(() => {
+    fetch("./walkstarts.json")
+      .then(response => response.text())
+      .then(responseText => {
+        setWalkRoutes(JSON.parse(responseText));
+      });
+  }, []);
+
+  // const onBoundsChanged = ({ center, zoom, bounds, initial }) => {
+  //   console.log(center, zoom, bounds, initial)
+  // }
+
+  return (<>
     <section className="lake-map-container">
-      <Map defaultCenter={center} defaultZoom={10}>
-        {wainwrights?.map((hill, index) => {
-          return (<Marker key={index} anchor={[hill.latitude, hill.longitude]} color="red"/>)
+      <Map
+        // provider={maptilerProvider}
+        center={center} zoom={zoom} minZoom={10.5} zoomSnap={false}
+        // onBoundsChanged={onBoundsChanged}
+        >
+        {showWainwrights && wainwrights?.map((hill, index) => {
+          return (<Marker className="wainwright" key={index} anchor={[hill.latitude, hill.longitude]} color="#eb873c"/>)
         })}
+        {showWalkroutes && walkRoutes?.map((hill, index) => {
+          return (<Marker className="walks" key={index} anchor={[hill.latitude, hill.longitude]} color="#3ca0eb"/>)
+        })}
+        <ZoomControl />
       </Map>
     </section>
-  )
+  </>)
 }
