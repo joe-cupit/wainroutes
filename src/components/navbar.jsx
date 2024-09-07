1bbd7dd652ebc9c1c7f06937f8382ed926740826
Joe <jwcupit@gmail.com>
2024-09-07 17:04:25 +0100

    Simplify navbar markup and css (with drop-down for mobile)

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,49 +8,50 @@ export function Navbar() {
 
   const [darkMode, setDarkMode] = useState(false);
 
-  const toggleDarkMode = () => {
+  function toggleDarkMode() {
     setDarkMode(prev => !prev);
     if (darkMode) document.body.classList.remove('darkmode');
     else document.body.classList.add('darkmode');
   }
 
+  function toggleMobileNav() {
+    const nav = document.getElementById("navbar-nav");
+    if (nav.getAttribute("data-status") === "open") {
+      nav.setAttribute("data-status", "closed");
+    } else {
+      nav.setAttribute("data-status", "open");
+    }
+  }
+
   return (
-    <nav id="navbar" className="navbar font--urbanist">
-      <div className="navbar--left">
-        <button className="navbar--button navbar--mobile">
+    <header className="navbar flex-group flex-vertical-center flex-space">
+
+      <div className="navbar-left flex-group flex-vertical-center">
+        <button className="nav-button navbar-mobile-only" onClick={toggleMobileNav}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="size-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
           </svg>
         </button>
-        <Link className="navbar--home" to="/">wainroutes</Link>
-        <span className="navbar--links">
-          <Link className="navbar--link" to="/walks">walks</Link>
-          <Link className="navbar--link" to="/mountains">mountains</Link>
-          <Link className="navbar--link" to="/weather">weather</Link>
-          <Link className="navbar--link" to="/travel">travel</Link>          
-        </span>
+
+        <Link className="navbar-navtitle" to="/">wainroutes</Link>
+        <nav id="navbar-nav" className="navbar-nav flex-group">
+          <Link className="navbar-navlink" to="/walks">walks</Link>
+          <Link className="navbar-navlink" to="/mountains">mountains</Link>
+          <Link className="navbar-navlink" to="/weather">weather</Link>
+          <Link className="navbar-navlink" to="/travel">travel</Link>
+          <p className="navbar-mobile-only font-cursive text-light">"no pain, no wain!" - LT</p>
+        </nav>
       </div>
 
-      <div className="navbar--right">
-
-        <button className="navbar--button navbar--button-block navbar--dark-light" onClick={toggleDarkMode}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-          {darkMode
-          ? <path fillRule="evenodd" d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z" clipRule="evenodd" />
-          : <path d="M12 2.25a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-1.5 0V3a.75.75 0 0 1 .75-.75ZM7.5 12a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM18.894 6.166a.75.75 0 0 0-1.06-1.06l-1.591 1.59a.75.75 0 1 0 1.06 1.061l1.591-1.59ZM21.75 12a.75.75 0 0 1-.75.75h-2.25a.75.75 0 0 1 0-1.5H21a.75.75 0 0 1 .75.75ZM17.834 18.894a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 1 0-1.061 1.06l1.59 1.591ZM12 18a.75.75 0 0 1 .75.75V21a.75.75 0 0 1-1.5 0v-2.25A.75.75 0 0 1 12 18ZM7.758 17.303a.75.75 0 0 0-1.061-1.06l-1.591 1.59a.75.75 0 0 0 1.06 1.061l1.591-1.59ZM6 12a.75.75 0 0 1-.75.75H3a.75.75 0 0 1 0-1.5h2.25A.75.75 0 0 1 6 12ZM6.697 7.757a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 0 0-1.061 1.06l1.59 1.591Z" />
-          }
-          </svg>
-        </button>
-
-        <button className="navbar--button">
+      <div className="navbar-right flex-group">
+        <button className="nav-button nav-button-rotate">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="size-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
             <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
           </svg>
         </button>
-
       </div>
 
-    </nav>
+    </header>
   )
 }
