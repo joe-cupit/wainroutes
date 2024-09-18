bf038d424d53663799e3747dc9896bb4b5c408cb
Joe <jwcupit@gmail.com>
2024-09-19 00:32:08 +0100

    Fix incorrect map zoom on page load

--- src/components/map/index.jsx
+++ src/components/map/index.jsx
@@ -42,23 +42,23 @@ export function LakeMap ({ mapMarkers, gpxPoints, ...props }) {
       if (long > maxLong) maxLong = long;
     }
 
-    const mapBounds = document.getElementById("lake-map").getBoundingClientRect()
-    const { center, zoom } = geoViewport.viewport(
-      [minLat, minLong, maxLat, maxLong], [mapBounds.width, mapBounds.height], 0, 20, 256, true, true
-    )
+    // const mapBounds = document.getElementById("lake-map").getBoundingClientRect()
+    // const { center, zoom } = geoViewport.viewport(
+    //   [minLat, minLong, maxLat, maxLong], [mapBounds.width, mapBounds.height], 0, 20, 256, true, true
+    // )
 
-    setCenter([center[0]+(!gpxPoints ? 0.015 : 0), center[1]]);
-    setZoom(zoom*0.98);
-    setMinZoom(zoom*0.83);
+    // setCenter([center[0]+(!gpxPoints ? 0.015 : 0), center[1]]);
+    // setZoom(zoom*0.98);
+    // setMinZoom(zoom*0.83);
 
-    // const mapBounds = document.getElementById("lake-map").getBoundingClientRect();
-    // let newcenter = [(minLat+maxLat)/2, (minLong+maxLong)/2]
-    // let newzoom = -Math.max(Math.log((maxLat-minLat)/(mapBounds.width))/Math.log(2), Math.log((maxLong-minLong)/(mapBounds.height))/Math.log(2))
+    const mapBounds = document.getElementById("lake-map").getBoundingClientRect();
+    let newcenter = [(minLat+maxLat)/2, (minLong+maxLong)/2]
+    let newzoom = -Math.max(Math.log((maxLat-minLat)/(mapBounds.height-100))/Math.log(2), Math.log((maxLong-minLong)/(mapBounds.width))/Math.log(2))
 
-    // setCenter(newcenter);
-    // setZoom(newzoom);
+    setCenter(newcenter);
+    setZoom(newzoom*0.98);
 
-  }, [mapPoints]);
+  }, [mapPoints, gpxPoints]);
 
 
   const supercluster = useSupercluster(mapMarkers);
@@ -167,7 +167,7 @@ function HillIcon({ book }) {
 
 function WalkIcon() {
   return (
-    <svg className="lake-map--marker walk-marker" viewBox="0 0 24 30" xmlns="http://www.w3.org/2000/svg" fill="currentColor" stroke="#151515" strokeWidth={1} >
+    <svg className="lake-map--marker walk-marker" viewBox="4 1 15.5 22" xmlns="http://www.w3.org/2000/svg" fill="currentColor" stroke="#151515" strokeWidth={1} >
       <g style={{ pointerEvents: "auto" }}>
         <circle cx="12.5" cy="9.5" r="4" fill="rgba(222, 225, 178, .01)" />
         <path d="M13.0392019,21.7081936 C12.0940626,22.2815258 10.8626021,21.9809256 10.2886866,21.0367943 C6.7619497,15.2353103 5,11.2870891 5,8.99256161 C5,5.13067647 8.13400675,2 12,2 C15.8659932,2 19,5.13067647 19,8.99256161 C19,11.2870898 17.2380492,15.2353128 13.71131,21.0367998 C13.544473,21.3112468 13.3139409,21.5415339 13.0392019,21.7081936 Z M12.0074463,12 C13.666063,12 15.0106376,10.6568542 15.0106376,9 C15.0106376,7.34314575 13.666063,6 12.0074463,6 C10.3488296,6 9.00425503,7.34314575 9.00425503,9 C9.00425503,10.6568542 10.3488296,12 12.0074463,12 Z"/>
