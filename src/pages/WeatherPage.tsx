a9911057f370db11bef8140aa706d7bd1a90dd06
Joe <jwcupit@gmail.com>
2025-05-16 20:48:52 +0100

    Tweak weather page styles

--- src/pages/WeatherPage.tsx
+++ src/pages/WeatherPage.tsx
@@ -69,13 +69,13 @@ export function WeatherPage() {
     <main className="weather-page">
 
       <section>
-        <div className="flex-column weather-main">
-          <div className="weather-header">
+        <div className="flex-column weather__main">
+          <div className="weather__header">
             <h1 className="title">Lake District Weather</h1>
             <p className="secondary-text">{`Updated at: ${weatherData?.update_time}`}</p>
           </div>
 
-          <div className="flex-column weather-days">
+          <div className="flex-column weather__days">
             {weatherData?.days?.map((day, index) => {
               switch (day.type) {
                 // case "this-evening": return <EveningWeather key={index} weather={day} />
@@ -97,82 +97,67 @@ export function WeatherPage() {
 function CurrentDayWeather({ weather } : { weather : WeatherDay }) {
 
   return (
-    <div className="flex-column">
-      <div>
-        <span className="secondary-text">Sunrise: {weather.sunrise}, Sunset: {weather.sunset}</span>
-        <h2 className="heading">{DateTitle(weather.date)}</h2>
-      </div>
-
-      <div className="flex-row">
-        <div className="flex-column weather-day">
-          <p>{weather.weather}</p>
-
-          <div className="forecast">
-            <h3>Mountain Weather Forecast (800m)</h3>
-            <ForecastTable forecast={weather.forecast} />
-          </div>
-
-          <div>
-            <h3>Chance of Cloud-Free Hill Top</h3>
-            <p>{weather.cloud_free_top}</p>
-          </div>
-
-          <div>
-            <h3>Visibility</h3>
-            <p>{weather.visibility}</p>
-          </div>
-
-          <div>
-            <h3>Summary</h3>
-            <p>{weather.summary}</p>
-          </div>
+    <div className="weather__current-day">
+      <div className="flex-column">
+        <div>
+          <h2 className="heading">{DateTitle(weather.date)}</h2>
+          <p className="secondary-text">Sunrise: {weather.sunrise}, Sunset: {weather.sunset}</p>
         </div>
 
-        <div className="flex-column weather-extra-details">
-          <div className="flex-column mountain-hazards">
-            <h3>Mountain Hazards</h3>
+          <div className="weather__day">
+            <p>{weather.weather}</p>
 
-            {weather.hazards
-              ? <div className="flex-column mountain-hazards-list">
-                  {weather.hazards.high &&
-                    <div>
-                      <h4>High likelihood</h4>
-                      {Object.keys(weather.hazards.high).map((hazard, index) => {
-                        return <p key={index} className="mountain-hazard high">{hazard}</p>
-                      })}
-                    </div>
-                  }
-                  {weather.hazards.medium &&
-                    <div>
-                      <h4>Medium likelihood</h4>
-                      {Object.keys(weather.hazards.medium).map((hazard, index) => {
-                        return <p key={index} className="mountain-hazard medium">{hazard}</p>
-                      })}
-                    </div>
-                  }
-                  {weather.hazards.low &&
-                    <div>
-                      <h4>Low likelihood</h4>
-                      {Object.keys(weather.hazards.low).map((hazard, index) => {
-                        return <p key={index} className="mountain-hazard low">{hazard}</p>
-                      })}
-                    </div>
-                  }
-                </div>
-              : "None reported"}
-          </div>
+            <div className="weather__forecast">
+              <h3>Mountain Weather Forecast (800m)</h3>
+              <ForecastTable forecast={weather.forecast} />
+            </div>
 
-          <div>
-            <h3>Meteorologist's View</h3>
-            {weather.meteorologist_view && !weather.meteorologist_view.startsWith("Nothing")
-              ? <p className="meteorologist">{weather.meteorologist_view}</p>
-              : <p>{weather.meteorologist_view ?? "Nothing to add"}</p>
-            }
+            <div>
+              <h3>Chance of Cloud-Free Hill Top</h3>
+              <p>{weather.cloud_free_top}</p>
+            </div>
+
+            <div>
+              <h3>Visibility</h3>
+              <p>{weather.visibility}</p>
+            </div>
+
+            <div>
+              <h3>Summary</h3>
+              <p>{weather.summary}</p>
+            </div>
           </div>
+      </div>
+
+      <div className="weather__extra-details">
+        <div className="weather__mountain-hazards">
+          <h3 className="subheading">Mountain Hazards</h3>
+
+          {weather.hazards
+            ? <ul className="weather__mountain-hazards-list">
+                {weather.hazards.high && Object.keys(weather.hazards.high).map((hazard, index) => {
+                  return <li key={index} className="weather__mountain-hazard high">{hazard}</li>
+                })}
+                {weather.hazards.medium && Object.keys(weather.hazards.medium).map((hazard, index) => {
+                  return <li key={index} className="weather__mountain-hazard medium">{hazard}</li>
+                })}
+                {weather.hazards.low && Object.keys(weather.hazards.low).map((hazard, index) => {
+                  return <li key={index} className="weather__mountain-hazard low">{hazard}</li>
+                })}
+              </ul>
+            : "None reported"
+          }
+        </div>
+
+        <div className="weather__mountain-hazards">
+          <h3 className="subheading">Meteorologist's View</h3>
+          {weather.meteorologist_view && !weather.meteorologist_view.startsWith("Nothing")
+            ? <p className="weather__meteorologist-view">{weather.meteorologist_view}</p>
+            : <p>{weather.meteorologist_view ?? "Nothing to add"}</p>
+          }
         </div>
 
       </div>
-
     </div>
   )
 }
@@ -209,7 +194,7 @@ function ForecastTable({ forecast } : { forecast: DayForecast | undefined }) {
   if (forecast === undefined) return <></>
 
   return (
-    <table className="forecast-table">
+    <table className="weather__forecast-table">
       <thead>
         <tr>
           <th></th>
@@ -233,10 +218,10 @@ function ForecastTable({ forecast } : { forecast: DayForecast | undefined }) {
 
 function TomorrowsWeather({ weather } : { weather: WeatherDay }) {
   return (
-    <div className="flex-column weather-day">
+    <div className="flex-column weather__day">
       <div>
-        <span className="secondary-text">Sunrise: {weather.sunrise}, Sunset: {weather.sunset}</span>
         <h2 className="heading">{DateTitle(weather.date)}</h2>
+        <p className="secondary-text">Sunrise: {weather.sunrise}, Sunset: {weather.sunset}</p>
       </div>
 
       <p>{weather.summary}</p>
@@ -279,21 +264,27 @@ function FutureWeather({ weather } : { weather: WeatherDay }) {
   } as { [shorthand : string] : string }
 
   return (
-    weather.days?.map((day, index) => {
-      var dateList = day.date.split(" ");
-      dateList[0] = weekdayDict[dateList[0]];
-      dateList[2] = monthDict[dateList[2]];
+    <div>
+      <h2 className="heading">Further Outlook</h2>
 
-      return (
-        <div key={index} className="flex-column future weather-day">
-          <div>
-            <span className="secondary-text">Sunrise: {day.sunrise}, Sunset: {day.sunset}</span>
-            <h2 className="subheading">{dateList.join(" ")}</h2>
-          </div>
-          <p>{day.summary}</p>
-        </div>
-      )
-    })
+      <div className="weather__further-days">
+        {weather.days?.map((day, index) => {
+          var dateList = day.date.split(" ");
+          dateList[0] = weekdayDict[dateList[0]];
+          dateList[2] = monthDict[dateList[2]];
+    
+          return (
+            <div key={index} className="flex-column future weather__day">
+              <div>
+                <h3 className="subheading">{dateList.join(" ")}</h3>
+                <p className="secondary-text">Sunrise: {day.sunrise}, Sunset: {day.sunset}</p>
+              </div>
+              <p>{day.summary}</p>
+            </div>
+          )
+        })}
+      </div>
+    </div>
   )
 }
 
