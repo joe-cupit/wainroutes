36a6008bcfd09e8a19d4c19e219a8d35e0d47ae8
Joe <jwcupit@gmail.com>
2025-06-04 20:06:20 +0100

    Update walk weather component layout

--- src/pages/WalkPage/components/Weather.tsx
+++ src/pages/WalkPage/components/Weather.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useMemo, useState } from "react";
 import { Link } from "react-router-dom";
 
-import { displayDistance, displayElevation, displaySpeed, displayTemperature } from "../../../utils/unitConversions";
+import { displayElevation, displaySpeed, displayTemperature, getDistanceUnit, getDistanceValue } from "../../../utils/unitConversions";
 import { usePointWeather } from "../../../contexts/WeatherContext";
 
 
@@ -42,77 +42,79 @@ export function Weather({ secRef, weatherLoc } : { secRef: React.RefObject<HTMLD
 
       {weatherData
       ? <>
-          <p>Forecast for <b>{weatherLoc} ({displayElevation(weatherData?.elevation)})</b></p>
+          <p>Forecast for <b>{weatherLoc} ({displayElevation(weatherData.elevation)})</b></p>
 
           <div className="walk-page_weather-tabs flex-row justify-apart">
-            {weatherData?.days?.map((day, index: number) => {
-              let dayDate = new Date(day?.date)
+            {weatherData.days.map((day, index: number) => {
+              let dayDate = new Date(day.date)
 
               return (
                 <button key={index}
-                  className={"walk-page_weather-tab flex-column gap-0 align-center smallheading" + ([0, 6].includes(dayDate?.getDay()) ? " weekend" : "") + (index === selectedDayIndex ? " selected" : "")}
+                  className={"walk-page_weather-tab flex-column gap-0 align-center smallheading" + ([0, 6].includes(dayDate.getDay()) ? " weekend" : "") + (index === selectedDayIndex ? " selected" : "")}
                   onClick={() => setSelectedDayIndex(index)}
                 >
-                  <span className="subtext">{days[dayDate?.getDay()]}</span>
-                  <span className="subtext weather-day">{dayDate?.getDate()}</span>
+                  <span className="subtext">{days[dayDate.getDay()]}</span>
+                  <span className="subtext weather-day">{dayDate.getDate()}</span>
                 </button>
               )
             })}
           </div>
 
-          <div className={"walks-page_section walks-page_weather-body flex-column" + (displayNightData ? " night-view" : "")}>
-            <div className="walks-page_weather-header flex-row justify-apart align-center">
-              <h3 className="subheading walks-page_weather-date">{formatDate(selectedDayWeather?.date)}</h3>
-              <div className="walks-page_day-night">
-                <button className={!displayNightData ? "active" : ""} onClick={() => setDisplayNightData(0)}>day</button>
-                &nbsp;/&nbsp;
-                <button className={displayNightData ? "active" : ""} onClick={() => setDisplayNightData(1)}>night</button>
+          {selectedDayWeather &&
+            <div className={"walks-page_section walks-page_weather-body flex-column" + (displayNightData ? " night-view" : "")}>
+              <div className="walks-page_weather-header flex-row justify-apart align-center">
+                <h3 className="subheading walks-page_weather-date">{formatDate(selectedDayWeather.date)}</h3>
+                <div className="walks-page_day-night">
+                  <button className={!displayNightData ? "active" : ""} onClick={() => setDisplayNightData(0)}>day</button>
+                  &nbsp;/&nbsp;
+                  <button className={displayNightData ? "active" : ""} onClick={() => setDisplayNightData(1)}>night</button>
+                </div>
               </div>
-            </div>
 
-            <div className="walks-page_weather-main flex-row align-center">
-              <div className="flex-row gap-0 align-center">
-                <div className="flex-column gap-0">
-                  <span className="walks-page_weather-temp title">{displayTemperature(selectedDayWeather?.temp?.screen?.[displayNightData])}</span>
+              <div className="walks-page_weather-main flex-row align-center">
+                <div className="flex-row gap-0 align-center">
+                  <div className="flex-column gap-0">
+                    <span className="walks-page_weather-temp title">{displayTemperature(selectedDayWeather.temp.screen[displayNightData])}</span>
 
-                  <div className="walks-page_weather-temps">
-                    L: {displayTemperature(selectedDayWeather?.temp?.min?.[displayNightData], false)} H: {displayTemperature(selectedDayWeather?.temp?.max?.[displayNightData], false)}
+                    <div className="walks-page_weather-temps">
+                      L: {displayTemperature(selectedDayWeather.temp.min[displayNightData], false)} H: {displayTemperature(selectedDayWeather.temp.max[displayNightData], false)}
+                    </div>
+                  </div>
+
+                  <div className="walks-page_weather-symbol">
+                    <img src={WeatherSymbols[`${(selectedDayWeather.weather_type[displayNightData]).toLowerCase().replaceAll(" ", "-").replaceAll(/[()]/g, "")}.svg`]} alt={selectedDayWeather.weather_type[displayNightData]} title={selectedDayWeather.weather_type[displayNightData]} />
                   </div>
                 </div>
 
-                <div className="walks-page_weather-symbol">
-                  <img src={WeatherSymbols[`${(selectedDayWeather?.weather_type?.[displayNightData])?.toLowerCase().replaceAll(" ", "-").replaceAll(/[()]/g, "")}.svg`]} alt={selectedDayWeather?.weather_type?.[displayNightData]} title={selectedDayWeather?.weather_type?.[displayNightData]} />
+                <div className="walks-page_weather-grid grid-two gap-0">
+                  <div className="walks-page_weather-grid_box">
+                    <h4>Feels-like</h4>
+                    <p>{displayTemperature(selectedDayWeather.temp.feels[displayNightData], false)}</p>
+                  </div>
+                  <div className="walks-page_weather-grid_box">
+                    <h4>{"Chance of " + (selectedDayWeather.precipitation.type[displayNightData] ?? "precipitation")}</h4>
+                    <p>{selectedDayWeather.precipitation.prob[displayNightData] + "%"}</p>
+                  </div>
+                  <div className="walks-page_weather-grid_box">
+                    <h4>Wind speed (gusts)</h4>
+                    <p>{
+                      displaySpeed(selectedDayWeather.wind.speed[displayNightData])
+                      + " (" + displaySpeed(selectedDayWeather.wind.gusts[displayNightData]) + ")"
+                    }</p>
+                  </div>
+                  <div className="walks-page_weather-grid_box">
+                    <h4>Visibility</h4>
+                    <p className="default-value">{selectedDayWeather.visibility.text[displayNightData]}</p>
+                    <p className="hover-value">{DisplayVisibilityDistance(selectedDayWeather.visibility.m[displayNightData])}</p>
+                  </div>
                 </div>
               </div>
 
-              <div className="walks-page_weather-grid grid-two gap-0">
-                <div className="walks-page_weather-grid_box">
-                  <h4>Feels-like</h4>
-                  <p>{displayTemperature(selectedDayWeather?.temp?.feels?.[displayNightData], false)}</p>
-                </div>
-                <div className="walks-page_weather-grid_box">
-                  <h4>Precipitation</h4>
-                  <p className="default-value">{selectedDayWeather?.precipitation?.prob?.[displayNightData] + "%"}</p>
-                  <p className="hover-value">{selectedDayWeather?.precipitation?.type?.[displayNightData]}</p>
-                </div>
-                <div className="walks-page_weather-grid_box">
-                  <h4 className="default-value">Wind speed</h4>
-                  <h4 className="hover-value">Wind gusts</h4>
-                  <p className="default-value">{displaySpeed(selectedDayWeather?.wind?.speed?.[displayNightData])}</p>
-                  <p className="hover-value">{displaySpeed(selectedDayWeather?.wind?.gusts?.[displayNightData])}</p>
-                </div>
-                <div className="walks-page_weather-grid_box">
-                  <h4>Visibility</h4>
-                  <p className="default-value">{selectedDayWeather?.visibility?.text?.[displayNightData]}</p>
-                  <p className="hover-value">{displayDistance((selectedDayWeather?.visibility?.m?.[displayNightData] ?? 0) / 1000, 0)}</p>
-                </div>
-              </div>
+              <p className="walks-page_weather-info subtext">
+                Provided by the <a href="https://www.metoffice.gov.uk/" target="_blank">Met Office</a> under the <a href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/" target="_blank">Open Government Licence</a>.
+              </p>
             </div>
-
-            <p className="walks-page_weather-info subtext">
-              Provided by the <a href="https://www.metoffice.gov.uk/" target="_blank">Met Office</a> under the <a href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/" target="_blank">Open Government Licence</a>.
-            </p>
-          </div>
+          }
         </>
       : <p>The forecast for this walk is currently unavailable.</p>
       }
@@ -120,4 +122,22 @@ export function Weather({ secRef, weatherLoc } : { secRef: React.RefObject<HTMLD
       <p className="walks-page_weather-link">For the district-wide forecast, see the <Link to="/weather">mountain weather forecast</Link>.</p>
     </div>
   )
+}
+
+
+function DisplayVisibilityDistance(meters: number ) {
+  const convertedValue = getDistanceValue(meters / 1000);
+  const distanceUnit = getDistanceUnit(true);
+
+  if (convertedValue == undefined) return "N/A" + distanceUnit;
+
+  if (convertedValue < 1) {
+    return "<1" + distanceUnit;
+  }
+  else if (convertedValue <= 5) {
+    return convertedValue.toFixed(0) + distanceUnit;
+  }
+  else {
+    return (Math.floor(convertedValue / 5) * 5).toFixed(0) + distanceUnit + "+";
+  }
 }
--- src/utils/unitConversions.ts
+++ src/utils/unitConversions.ts
@@ -1,51 +1,48 @@
-const useMiles = false
-const useFeet = false
-const useFarenheit = false
+const useMiles = false;
+const useFeet = false;
+const useFarenheit = false;
 
 
 export function getDistanceValue(kilometers : number | undefined, roundTo=4) {
-  if (!kilometers) return 0;
+  if (kilometers == undefined) return null;
 
   if (useMiles) return Number((kilometers / 1.609344).toFixed(roundTo));
   else return Number(Number(kilometers).toFixed(roundTo));
 }
-export function getDistanceUnit() {
-  if (useMiles) return "mi"
-  else return "km"
+export function getDistanceUnit(long=false) {
+  if (useMiles) return long ? "miles" : "mi";
+  else return "km";
 }
 export function getSpeedUnit() {
-  if (useMiles) return "mph"
-  else return "kph"
+  if (useMiles) return "mph";
+  else return "kph";
 }
 export function displayDistance(kilometers : number | undefined, roundTo=2) {
-  return getDistanceValue(kilometers).toFixed(roundTo) + getDistanceUnit()
+  return (getDistanceValue(kilometers)?.toFixed(roundTo) ?? "N/A") + getDistanceUnit();
 }
 export function displaySpeed(kilometers_per_hour : number | undefined, roundTo=0) {
-  return getDistanceValue(kilometers_per_hour).toFixed(roundTo) + getSpeedUnit()
+  return (getDistanceValue(kilometers_per_hour)?.toFixed(roundTo) ?? "N/A") + getSpeedUnit();
 }
 
 
-export function metersToFeet(meters : number | undefined) {
-  if (!meters) return 0;
-
-  return Number((meters * 3.280839895).toFixed(0))
-}
 export function getElevationValue(meters : number | undefined) {
-  if (useFeet) return metersToFeet(meters)
-  else return Number(Number(meters).toFixed(0))
+  if (meters == undefined) return null;
+
+  if (useFeet) return Number((meters * 3.280839895).toFixed(0));
+  else return Number(meters.toFixed(0));
 }
 export function getElevationUnit() {
-  if (useFeet) return "ft"
-  else return "m"
+  if (useFeet) return "ft";
+  else return "m";
 }
 export function displayElevation(meters : number | undefined) {
-  return getElevationValue(meters).toFixed(0) + getElevationUnit()
+  return (getElevationValue(meters)?.toFixed(0) ?? "N/A") + getElevationUnit();
 }
 
 
 export function displayTemperature(celcius : number | undefined, includeUnit=true) {
-  if (!celcius) return 0;
+  if (celcius == undefined) return "N/A";
 
-  if (useFarenheit) return `${((celcius * 9 / 5) + 32).toFixed(0)}°` + (includeUnit ? "F" : "")
-  else return `${celcius.toFixed(0)}°` + (includeUnit ? "C" : "")
+  if (useFarenheit) return `${((celcius * 9 / 5) + 32).toFixed(0)}°` + (includeUnit ? "F" : "");
+  else return `${celcius.toFixed(0)}°` + (includeUnit ? "C" : "");
 }
