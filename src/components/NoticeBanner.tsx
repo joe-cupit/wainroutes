f521d2c91f572ecd69b3b3da79b5482cf00e8ddc
joe-cupit <jwcupit@gmail.com>
2025-07-09 10:59:43 +0100

    Add close button to notice banner and update message

--- src/components/NoticeBanner.tsx
+++ src/components/NoticeBanner.tsx
@@ -1,10 +1,39 @@
+import { useCallback, useEffect, useState } from 'react';
+import { PlusIcon } from './Icons';
 import './NoticeBanner.css';
 
 
 export default function NoticeBanner() {
-  return (
-    <div className='notice-banner'>
-      <p>This site is still a work in progress, please send any issues to <a href='mailto:hello@wainroutes.co.uk'>hello@wainroutes.co.uk</a></p>
-    </div>
-  )
+
+  const [display, setDisplay] = useState(false);
+
+  useEffect(() => {
+    const shouldClose = localStorage.getItem("user-has-closed-banner");
+
+    if (shouldClose) setDisplay(false);
+    else setDisplay(true);
+  }, [])
+
+  const closeBanner = useCallback(() => {
+    console.log("helo")
+    localStorage.setItem("user-has-closed-banner", "true");
+    setDisplay(false);
+  }, [])
+
+
+  if (display) {
+    return (
+      <div className='notice-banner'>
+        <p>I'm still working to improve this site, please email any issues to <a href='mailto:joe@wainroutes.co.uk'>joe@wainroutes.co.uk</a></p>
+        <button
+          className='notice-banner__close'
+          title='Dismiss notice'
+          onClick={() => closeBanner()}
+        >
+          <PlusIcon />
+        </button>
+      </div>
+    )
+  }
+  else return <></>
 }
