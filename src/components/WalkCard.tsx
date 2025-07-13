bec4d8ebc3d4c3dfe1917580e5ace05bedb7f807
Joe <jwcupit@gmail.com>
2025-07-13 17:05:11 +0100

    Add responsive walk card layout

--- src/components/WalkCard.tsx
+++ src/components/WalkCard.tsx
@@ -12,45 +12,52 @@ import { displayDistance, displayElevation, getDistanceUnit, getDistanceValue } 
 export default function WalkCard({ walk, showDistance, hoverEvent } : { walk: Walk; showDistance?: boolean, hoverEvent?: CallableFunction }) {
   return (
     <article
-      className="walks-card"
+      className="walks-card__container"
       onMouseEnter={() => hoverEvent && hoverEvent(walk.slug)}
       onMouseLeave={() => hoverEvent && hoverEvent(null)}
     >
-      <div className="walks-card__image">
-        <Image
-          name={walk?.slug + "_" + walk?.gallery?.coverId}
-          sizes="(min-width: 22rem) 22rem, 100vw"
-          maxWidth={512}
-        />
-        {(showDistance && walk.distance) &&
-          <div className="walks-card__dist">
-            {getDistanceValue(walk.distance) < 1
-              ? "Less than 1" + getDistanceUnit()
-              : displayDistance(walk.distance, 1) + " away"
-            }
-          </div>
-        }
-      </div>
-
-      <div className="walks-card__text">
-        <h3 className="subheading">{walk.title}</h3>
-        <div className="walks-card__icons flex-row">
-          <div className="flex-row gap-0 align-center">
-            <HikingIcon />
-            {displayDistance(walk.length, 1)}
-          </div>
-          <div className="flex-row gap-0 align-center">
-            <ElevationIcon />
-            {displayElevation(walk.elevation)}
-          </div>
-          <div className="flex-row gap-0 align-center">
-            <MountainIcon />
-            {walk.wainwrights?.length ?? "N/A"}
-          </div>
+      <Link to={"/walks/"+walk.slug} className="walks-card">
+        <div className="walks-card__image">
+          <Image
+            name={walk?.slug + "_" + walk?.gallery?.coverId}
+            sizes="(min-width: 22rem) 22rem, 100vw"
+            maxWidth={512}
+          />
+          {(showDistance && walk.distance) &&
+            <div className="walks-card__dist">
+              {getDistanceValue(walk.distance) < 1
+                ? "Less than 1" + getDistanceUnit()
+                : displayDistance(walk.distance, 1) + " away"
+              }
+            </div>
+          }
         </div>
-
-        <Link to={"/walks/"+walk.slug} className="button">View walk</Link>
-      </div>
+        <div className="walks-card__text">
+          <div>
+            <h3 className="subheading">{walk.title}</h3>
+            <p>This is a lovely walk, with a simple-ish description of the route and some cool things that you will be able to see on the route.</p>
+          </div>
+          <div className="walks-card__icons">
+            <div className="walks-card__icons-icon">
+              <HikingIcon />
+              {displayDistance(walk.length, 1)}
+            </div>
+            <div className="walks-card__icons-icon">
+              <ElevationIcon />
+              {displayElevation(walk.elevation)}
+            </div>
+            <div className="walks-card__icons-icon">
+              {walk.wainwrights?.length ?? "0"}
+              <MountainIcon />
+              {/* {walk.wainwrights?.length
+                ? walk.wainwrights?.length + " Wainwright" + (walk.wainwrights?.length === 1 ? "" : "s")
+                : "0"
+              } */}
+            </div>
+          </div>
+          {/* <Link to={"/walks/"+walk.slug} className="button">View walk</Link> */}
+        </div>
+      </Link>
     </article>
   )
 }
