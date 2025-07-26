a8ac97f623967e2784f9747ff7bc2dccabc73322
Joe <jwcupit@gmail.com>
2025-07-26 22:21:53 +0100

    Add Open Graph and twitter meta tags to head

--- src/components/WainroutesHelmet.tsx
+++ src/components/WainroutesHelmet.tsx
@@ -1,12 +1,40 @@
+import { useMemo } from 'react';
 import { Helmet } from 'react-helmet'
 
-export default function WainroutesHelmet({ title, description, canonical } : { title?: string; description?: string; canonical?: string }) {
-  
+
+export default function WainroutesHelmet({ title, description, canonical, imageUrl } : { title?: string; description?: string; canonical?: string, imageUrl?: string }) {
+
+  const fullTitle = useMemo(() => {
+    if (title && title.length > 0) return title + " | Wainroutes";
+    else return "Wainroutes";
+  }, [title])
+
+  const canonicalUrl = useMemo(() => {
+    if (canonical !== undefined) return "https://wainroutes.co.uk" + canonical;
+    else return null;
+  }, [canonical])
+
+
   return (
     <Helmet>
-      <title>{title ? title + " | Wainroutes" : "Wainroutes"}</title>
-      {description && <meta name="description" content={description} />}
-      {(canonical !== undefined) && <link rel="canonical" href={"https://wainroutes.co.uk" + canonical} />}
+      <title>{fullTitle}</title>
+      <meta property="og:title" content={fullTitle} />
+      <meta name="twitter:title" content={fullTitle} />
+
+      {description && [
+        <meta name="description" content={description} />,
+        <meta property="og:description" content={description} />,
+        <meta name="twitter:description" content={description} />
+      ]}
+      {canonicalUrl && [
+        <link rel="canonical" href={canonicalUrl} />,
+        <meta property="og:url" content={canonicalUrl} />
+      ]}
+
+      {imageUrl && [
+        <meta property="og:image" content={imageUrl} />,
+        <meta name="twitter:image" content={imageUrl} />
+      ]}
     </Helmet>
   )
 }
