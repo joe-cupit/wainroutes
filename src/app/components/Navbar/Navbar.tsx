36f79b09ac41e3432abd7d9498389f28c2ebc5db
Joe <jwcupit@gmail.com>
2025-08-03 13:57:31 +0100

    Add navbar client interactions

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -3,23 +3,27 @@ import fonts from "@/app/fonts.module.css";
 
 import Link from "next/link";
 
+import MobileNavbarButton from "./components/MobileNavbarButton";
+import NavbarClient from "./NavbarClient";
 
-export default function Navbar() {
+const navbarId = "navbar";
+
+
+export default async function Navbar() {
+
 
   return (
     <>
       <header
-        className={styles.navbar}
+        id={navbarId}
+        className={`${styles.navbar}`}
       >
+        <NavbarClient targetId={navbarId} />
 
         <div className={styles.main}>
 
           <div className={styles.mainLeft}>
-            <button className={`${styles.button} ${styles.mobileOnly}`}>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="size-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
-              </svg>
-            </button>
+            <MobileNavbarButton />
 
             <Link href="/" className={fonts.heading}>wainroutes</Link>
           </div>
@@ -34,24 +38,6 @@ export default function Navbar() {
           </div>
         </div>
       </header>
-
-      <div id="navbar-mobile" className={styles.mobile}>
-        <button className={styles.button}>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="5 5 18 18" strokeWidth="2" stroke="currentColor">
-            <path d="M6 18 18 6M6 6l12 12" strokeLinecap="round" strokeLinejoin="round" />
-          </svg>
-        </button>
-
-        <nav>
-          <ul className={fonts.heading}>
-            <li><Link href="/">home</Link></li>
-            <li><Link href="/walks">walks</Link></li>
-            <li><Link href="/wainwrights">wainwrights</Link></li>
-            <li><Link href="/weather">weather</Link></li>
-            <li><Link href="/travel">travel</Link></li>
-          </ul>
-        </nav>
-      </div>
     </>
   )
 }
---
+++ src/app/components/Navbar/components/MobileNavbar.tsx
@@ -0,0 +1,61 @@
+"use client"
+
+import styles from "../Navbar.module.css";
+import fonts from "@/app/fonts.module.css";
+
+import { Dispatch, SetStateAction, useCallback, useEffect } from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const id = "navbar-mobile";
+
+
+export default function MobileNavbar({ open, setOpen } : { open: boolean; setOpen: Dispatch<SetStateAction<boolean>> }) {
+
+  const pathname = usePathname();
+  useEffect(() => {
+    setOpen(false);
+  }, [pathname, setOpen])
+
+  const clickListener = useCallback((e: Event) => {
+    const mobileNav = document.getElementById(id);
+    if (mobileNav && !e.composedPath().includes(mobileNav)) {
+      setOpen(false);
+    }
+  }, [setOpen])
+
+
+  useEffect(() => {
+    if (!open) return;
+
+    document.addEventListener("mouseup", clickListener);
+    return () => {
+      document.removeEventListener("mouseup", clickListener);
+    }
+  }, [open, clickListener])
+
+
+  return (
+    <div
+      id={id}
+      className={`${styles.mobile} ${open ? styles.active : ""}`}
+      aria-expanded={open}
+    >
+      <button className={styles.button} onClick={() => setOpen(false)}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" style={{scale: 1.2}}>
+          <path d="M6 18 18 6M6 6l12 12" strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+      </button>
+
+      <nav>
+        <ul className={fonts.heading}>
+          <li><Link href="/">home</Link></li>
+          <li><Link href="/walks">walks</Link></li>
+          <li><Link href="/wainwrights">wainwrights</Link></li>
+          <li><Link href="/weather">weather</Link></li>
+          <li><Link href="/travel">travel</Link></li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
