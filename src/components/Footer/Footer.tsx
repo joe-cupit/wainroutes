509996e8808b6e6a3c708802168a114aa44feeba
Joe <jwcupit@gmail.com>
2025-09-11 16:17:13 +0100

    Add mountain svg to footer and restyle its components

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,45 +1,63 @@
 import styles from "./Footer.module.css";
 import fonts from "@/styles/fonts.module.css";
 
+import WainroutesLogo from "@/components/Logo/Logo";
 import SiteSearchBar from "@/components/SiteSearchBar/SiteSearchBar";
-import Link from "next/link";
+import LinkList from "./components/LinkList";
+import Mountains from "./components/Mountains";
 
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
+      <Mountains />
+
       <div className={styles.grid}>
-        <div className={styles.main}>
-          <div className={styles.mainLeft}>
+
+        <div className={styles.row}>
+          <div className={styles.logo}>
+            <WainroutesLogo />
             <h2 className={fonts.heading}>wainroutes</h2>
+          </div>
+        </div>
+
+        <div className={`${styles.row} ${styles.wrappable}`}>
+          <div className={styles.search}>
+            <h2 className={fonts.smallheading}>Search for a walk</h2>
             <SiteSearchBar
               reversed={true}
               small={true}
               className={styles.searchBar}
-              placeholder="Find a walk"
+              placeholder="Search for a route, fell, or town"
             />
           </div>
-          <div className={styles.mainRight}>
-            <div className={styles.mainLinks}>
-              <h3 className={fonts.subheading}>Explore</h3>
-              <Link href="/walks">Walks</Link>
-              <Link href="/walks">Map</Link>
-              <Link href="/wainwrights">Wainwrights</Link>
-              <Link href="/weather">Weather</Link>
-            </div>
-            <div className={styles.mainLinks}>
-              <h3 className={fonts.subheading}>Discover</h3>
-              <Link href="/">Home</Link>
-              <Link href="/about">About</Link>
-              <Link href="/contact">Contact</Link>
-            </div>
+
+          <div className={styles.group}>
+            <LinkList
+              heading="Plan a trip"
+              links={{
+                "Find a walk": "/walks",
+                "Weather": "/weather",
+                "Wainwrights": "/wainwrights",
+                "Safety": "/safety",
+                "Travel": "/travel",
+              }}
+            />
+            <LinkList
+              heading="Wainroutes"
+              links={{
+                "Home": "/",
+                "About": "/about",
+                "Contact": "/contact",
+                "Donate": "/about/support",
+              }}
+            />
           </div>
         </div>
 
-        <div className={styles.base}>
+        <div className={`${styles.row} ${styles.base}`}>
           <p>&#169; 2025 Wainroutes</p>
           <p>A site by Joe Cupit.</p>
-          {/* <p>A site by <a href="https://www.google.com" target="_blank">Joe Cupit</a>.</p> */}
         </div>
       </div>
     </footer>
---
+++ src/components/Footer/components/LinkList.tsx
@@ -0,0 +1,27 @@
+import styles from "../Footer.module.css";
+import fonts from "@/styles/fonts.module.css";
+
+import Link from "next/link";
+
+
+type LinkListProps = {
+  heading: string
+  links: {[name: string] : string}
+}
+
+
+export default function LinkList({ heading, links } : LinkListProps) {
+
+  return (
+    <div className={styles.links}>
+      <h2 className={fonts.smallheading}>{heading}</h2>
+      <ul>
+        {Object.entries(links).map(([name, link], index) => (
+          <li key={index}>
+            <Link href={link}>{name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
